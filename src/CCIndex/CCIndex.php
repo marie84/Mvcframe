d49fb86b598fe640146fa74a206ae5eabaf651c3
Marie Svensson <marie.sve@hotmail.com>
2014-01-03 21:18:27 +0100

    Initial commit

--- src/CCIndex/CCIndex.php
+++ src/CCIndex/CCIndex.php
@@ -9,10 +9,32 @@ class CCIndex implements IController {
   /**
     * Implementing interface IController. All controllers must have an index action.
    */
-  public function Index() {  
-    global $mv;
-    $mv->data['title'] = "The Index Controller";
-    $mv->data['main'] = "<h1>The Index Controller</h1>";
+  public function Index() { 
+$this->Menu();
+        }
+		
+		/**
+          * Create a method that shows the menu, same for all methods
+         */
+        private function Menu() {        
+                $mv = CMvcframe::Instance();
+                $menu = array('index', 'index/index', 'developer', 'developer/index', 'developer/links');
+                
+                $html = null;
+                foreach($menu as $val) {
+                  $html .= "<li><a href='" . $mv->request->CreateUrl($val) . "'>$val</a>";  
+                }
+                
+                $mv->data['title'] = "The Index Controller";
+                $mv->data['main'] = <<<EOD
+<h1>The Index Controller</h1>
+<p>This is what you can do for now:</p>
+<ul>
+$html
+</ul>
+EOD;
   }
-
-}  
+  
+} 
+		
+    
